a56afed1a77f74e1af1ebf9587e4079c891c83bc
Mati2173 <matigomez2173@gmail.com>
2022-11-03 22:29:09 -0300

    Probando

---
+++ prueba.py
@@ -0,0 +1,10 @@
+Colaboradores = ["Macarena", "Rocio", "Walter", "Matias"]
+
+print('Proyecto: \'Cinemar\' - 1000 Programadores')
+
+print('Los colaboradores son:')
+i = 0
+for colaborador in Colaboradores:
+    i += 1
+    print(f'\t{i}. {colaborador}')
+
