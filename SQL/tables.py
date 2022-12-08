6208a73caebd86cf53febca7455f631ffb1fc5f2
SOLE-VME <sole.vme@gmail.com>
2022-12-08 01:01:29 -0300

    Peliculas cargadas

--- SQL/tables.py
+++ SQL/tables.py
@@ -2,6 +2,7 @@ import databases as db
 
 cm = db.DataBase("cinemar.db")
 
-cm.insert("peliculas","nombre,duracion,genero,tipo,director,actores,sinopsis","'Black Adam','170','Acción','3','Jaume Collet-Serra','Dwayne Johnson, Sarah Shahi, Pierce Brosnan, Viola Davis, Aldis Hodge, Noah Centineo, Chico Kenzari, Quintessa Swindell, Uli Latukefu, Bodhi Sabongui, Mo Amer, Tang Nguyen, Joseph Gatt, Angel Rosario Jr., Chaim Girafi, Jalon Christian, Cameron Moir, Rahiem Riley, Tre Ryan, Stephan Jones, Donny Carrington, D.J. Stavropoulos, Odelya Halevi, Natalie Burn','Casi 5.000 años después de haber sido dotado de los poderes omnipotentes de los antiguos dioses - y encarcelado con la misma rapidez -, Black Adam (Johnson) es liberado de su tumba terrenal, listo para desatar su forma única de justicia en el mundo moderno.'")
+cm.insert("peliculas","nombre,duracion,genero,tipo,director,actores,sinopsis","'Rapidos y furiosos 9','143','Acción','3','Justin Lin','Vin Diesel, Michelle Rodriguez, Jordana Brewster, Tyrese Gibson, Ludacris, John Cena, Charlize Theron, Helen Mirren, Kurt Russell, Sung Kang, Lucas Black, Finn Cole, Vinnie Bennett, Nathalie Emmanuel, Martyn Ford, Alexander Wraith, Bow Wow, Michael Rooker, Cardi B, Don Omar, Ozuna, Bad Bunny, JD Pardo, Anna Sawai, Thue Ersted Rasmussen, Shea Whigham, Jim Parrack, Igby Rigney, Siena Agudong, Jason Statham, Sophia Bui, Amber Sienna','Dom Toretto vive una vida tranquila junto a Letty y su hijo, pero el peligro siempre regresa a su vida. En esta ocasión, el equipo se enfrenta a un complot mundial orquestado por el asesino más temible del mundo: el hermano de Dom.'",)
+
 
 cm.close()
