985ebe09070bea51b5e931f185e3c40f06cd76fc
SOLE-VME <sole.vme@gmail.com>
2022-12-11 19:57:30 -0300

    modif clase funcion

--- Clases/Funcion.py
+++ Clases/Funcion.py
@@ -1,39 +1,63 @@
+from SQL import databases as db
+
 class Funcion:
     def __init__ (self,id,pelicula,horario,precio):
-        self.__id = id
-        self.__pelicula = pelicula
-        self.__horario = horario
-        self.__precio = precio
+        self.id = id
+        self.pelicula = pelicula
+        self.horario = horario
+        self.precio = precio
         
     @property
-    def id (self):
-        return self.__id
+    def id(self):
+        return self.id
 
     @id.setter
     def id(self,id):
-        self.__id = id
+        self.id = id
 
     @property
-    def pelicula (self):
-        return self.__pelicula
+    def pelicula(self):
+        return self.pelicula
 
     @pelicula.setter
     def pelicula(self,pelicula):
-        self.__pelicula = pelicula
+        self.pelicula = pelicula
 
 
     @property
-    def horario (self):
-        return self.__horario
+    def horario(self):
+        return self.horario
 
     @horario.setter
     def horario(self,horario):
-        self.__horario = horario
+        self.horario = horario
         
     @property
-    def precio (self):
-        return self.__precio
+    def precio(self):
+        return self.precio
 
     @precio.setter
     def precio(self,precio):
-        self.__precio = precio
+        self.precio = precio
+        
+    def __str__(self):
+        f"{self.id} {self.pelicula} {self.horario} {self.precio}"
+        
+
+    def agregarDbFunciones(conn, id_funcion, id_Pelicula, horario, precio): 
+        connection = conn
+        with connection.cursor() as cursor:
+            consulta = "INSERT INTO funciones (pelicula, horario, precio) VALUES (%s, %s, %s, %s);"
+            cursor.execute(consulta, (id_Pelicula, horario, precio))
+            connection.commit()
+            connection.close()
+        print("Función agregada correctamente")
+        
+    def eliminarBdfuncion(conn, id_funcion, id_Pelicula, horario, precio):
+        connection = conn
+        with connection.cursor() as cursor:
+             consulta = "DELETE FROM funciones WHERE(id)"
+             cursor.execute(consulta, (id_funcion, id_Pelicula, horario, precio))
+             connection.commit()
+             connection.close()
+        print("Función Eliminada")
