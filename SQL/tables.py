fc2c12feee07379b2225a9788d9b067a23874f07
Mati2173 <matigomez2173@gmail.com>
2022-12-12 11:59:12 -0300

    tablas definitivo

--- SQL/tables.py
+++ SQL/tables.py
@@ -1,7 +1,7 @@
 import databases as db
 
 cm = db.DataBase("cinemar.db")
-'''''
+
 cm.create_table("personas",
                 "id_persona INTEGER PRIMARY KEY AUTOINCREMENT,"+
                 "apellido TEXT(20),"+
@@ -33,13 +33,14 @@ cm.create_table("funciones",
                 "sala INTEGER,"+    #ID_Sala
                 "pelicula TEXT(20),"+   #Nombre Pelicula
                 "butacas_libres INTEGER,"+
+                "fecha TEXT(10),"+  #Formato --> YYYY-MM-DD
                 "horario TEXT(8),"+ #Formato --> HH:MM:SS   
                 "precio REAL,"+
                 "FOREIGN KEY(sala) REFERENCES salas(id_sala),"+
                 "FOREIGN KEY(pelicula) REFERENCES peliculas(nombre)")
 
 cm.create_table("salas",
-                "id_sala INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "id_sala INTEGER PRIMARY KEY,"+
                 "cant_butacas INTEGER,"+
                 "tipo TEXT(2)")    #(2D o 3D)
 
@@ -58,14 +59,5 @@ cm.create_table("descuentos",
                 "id_descuento INTEGER PRIMARY KEY AUTOINCREMENT,"+  #ID del día
                 "dia TEXT(10),"+    #Lunes, Martes, ...
                 "porcentaje INTEGER(3) DEFAULT 0")  
-'''
-cm.insert("descuentos","dia,porcentaje","'Lunes','20'")
-cm.insert("descuentos","dia,porcentaje","'Martes','15'")
-cm.insert("descuentos","dia,porcentaje","'Miercoles','20'")
-cm.insert("descuentos","dia,porcentaje","'Jueves','15'")
-cm.insert("descuentos","dia,porcentaje","'Viernes','20'")
-cm.insert("descuentos","dia,porcentaje","'Sabado','20'")
-cm.insert("descuentos","dia,porcentaje","'Domingo','10'")
-
 
 cm.close()
