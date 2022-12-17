8e2073a582824debda40a1e628fe1058c6bed56f
Mati2173 <matigomez2173@gmail.com>
2022-12-16 23:18:22 -0300

    Clase Ticket

--- Clases/Ticket.py
+++ Clases/Ticket.py
@@ -1,20 +1,30 @@
 from SQL import databases as db
 
 class Ticket():
-    def __init__(self, id_comprador = '', id_pelicula = '', cant_butacas = 0, fecha = '', horario = '', precio = 0, id = None):
+    def __init__(self, dni_comprador = 0, pelicula = '', cant_butacas = 0, fecha = '', horario = '', precio = 0, id = None):
         self.id = id
-        self.id_comprador = id_comprador
-        self.id_pelicula = id_pelicula
+        self.dni_comprador = dni_comprador
+        self.pelicula = pelicula
         self.cant_butacas = cant_butacas
         self.fecha = fecha
         self.horario = horario
         self.precio = precio
-
+    
     def cargar_ticket(self, bdd):
         bdd.insert('tickets',
                     'comprador, pelicula, cant_butacas, fecha, horario, precio', 
-                    f'{self.id_comprador}, {self.id_pelicula}, {self.cant_butacas}, "{self.fecha}", "{self.horario}", {self.precio}')
+                    f'{self.dni_comprador}, {self.pelicula}, {self.cant_butacas}, "{self.fecha}", "{self.horario}", {self.precio}')
     
     def all_tickets(self, bdd):
         lista = []
-        tickets =  bdd.select_all('tickets', '')
+        tickets =  bdd.select_all('tickets', 'id_ticket, comprador, pelicula, cant_butacas, fecha, horario, precio')
+
+        for i in range(len(tickets)):
+            lista.append(tickets[i])
+        
+        return lista
+    
+    def __str__(self):
+        ticket = f'ID Ticket: {self.id}\nComprador: {self.dni_comprador}\nCantidad de Butacas: {self.cant_butacas}'
+        ticket += f'Fecha: {self.fecha}\nHorario: {self.horario}\nPrecio: ${self.precio}'
+        return ticket
